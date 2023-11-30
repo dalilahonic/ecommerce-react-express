5c8911d35fce7511335c640ae039e1488ff53295
dalilahonic <dalilahonic1@gmail.com>
2023-11-30 12:47:36 +0100

    .

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,8 @@ import classes from './Cart.module.css';
 import HeaderCart from './HeaderCart';
 import Items from './Items';
 
+const tips = [10, 15, 20, 'other'];
+
 function Cart({
   orderInfo,
   setOrderInfo,
@@ -13,8 +15,8 @@ function Cart({
   return (
     <div className={classes.cart}>
       <div className={classes.cartChild}>
-        <ContinueShopping setIsCartOpen={setIsCartOpen} />
         <div className={classes.yourCartMain}>
+          <ContinueShopping setIsCartOpen={setIsCartOpen} />
           <HeaderCart amountCard={amountCard} />
           <Items
             orderInfo={orderInfo}
@@ -22,6 +24,35 @@ function Cart({
             setIsCartOpen={setIsCartOpen}
             setAmountCard={setAmountCard}
           />
+        </div>
+
+        <div className={classes.details}>
+          <div
+            className={`${classes.pickup} ${classes.info}`}
+          >
+            <div>
+              <h5>HOW TO GET IT</h5>
+              <p>Edit</p>
+            </div>
+            <div>
+              <p>Pickup:</p>
+              <p>Today:</p>
+            </div>
+          </div>
+
+          <div className={`${classes.tip} ${classes.info}`}>
+            <h5>ADD A TIP</h5>
+            <div className={classes.tips}>
+              {tips.map((el, i) => {
+                return (
+                  <div key={i}>
+                    {el}
+                    {typeof el === 'number' ? '%' : ''}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     </div>
